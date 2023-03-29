f16cd6a7c2b1c8f2ef7fd2f4c6d8f3f608adb366
Tun Putera Matin <tunputeramatin@TPM-MacBook-Air.local>
2023-03-29 16:11:24 +0800

    modified js task 2

--- index.js
+++ index.js
@@ -14,20 +14,27 @@ let offset = 0;
 const sliderLine = document.querySelector('.slider-line');
 
 document.querySelector('.slider-next').addEventListener('click', function(){
-offset = offset + 405;
-if (offset > 810) {
-    offset = 0;
-}
+offset = offset + 405 > 810 ? 0 : offset + 405;
+
 sliderLine.style.left = -offset + 'px';
 });
 
 document.querySelector('.slider-prev').addEventListener('click', function(){
-offset = offset - 405;
-if (offset < 0) {
-    offset = 810;
-}
+offset = offset - 405 < 0 ? 810 : offset - 405;
+
 sliderLine.style.left = -offset + 'px';
 });
 
+
+
+//Task #3 with star
+
+let year = 2023;
+if (((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0)) {
+    console.log ('високосный');
+}   
+else {
+    console.log('не високосный');
+}
 
 
